303dab81154973cab57e2ce21a2074529aa78403
Nomaan Ahgharian <code@nomaan.me>
2020-07-04 10:27:26 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,66 +1,61 @@
 module.exports = {
-  siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
-    hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
-      maxWidth: 652,
-    },
-    social: [
-      {
-        name: `twitter`,
-        url: `https://twitter.com/narative`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/narative`,
-      },
-      {
-        name: `instagram`,
-        url: `https://instagram.com/narative.co`,
-      },
-      {
-        name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
-      },
-    ],
-  },
-  plugins: [
-    {
-      resolve: "@narative/gatsby-theme-novela",
-      options: {
-        contentPosts: "content/posts",
-        contentAuthors: "content/authors",
-        basePath: "/",
-        authorsPage: true,
-        sources: {
-          local: true,
-          // contentful: true,
+    siteMetadata: {
+        title: `Nomaan Ahgharian`,
+        name: `Nomaan Ahgharian`,
+        siteUrl: `https://novela.narative.co`,
+        description: `Website of Nomaan Ahgharian - A Chicago based Software Engineer and UXer`,
+        hero: {
+            heading: `Hi, I'm Nomaan, a Chicago based Software Engineer with User Experience first approach `,
+            maxWidth: 652,
         },
-      },
+        social: [
+            {
+                name: `twitter`,
+                url: `https://twitter.com/UXNomaan`,
+            },
+            {
+                name: `github`,
+                url: `https://github.com/UXNomaan`,
+            },
+            {
+                name: `instagram`,
+                url: `https://instagram.com/UXNomaan`,
+            },
+            {
+                name: `linkedin`,
+                url: `https://www.linkedin.com/in/nomaan-ahgharian-0382a12a/`,
+            },
+        ],
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Novela by Narative`,
-        short_name: `Novela`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#fff`,
-        display: `standalone`,
-        icon: `src/assets/favicon.png`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-netlify-cms`,
-      options: {
-      },
-    },
-  ],
+    plugins: [
+        {
+            resolve: "@narative/gatsby-theme-novela",
+            options: {
+                contentPosts: "content/posts",
+                contentAuthors: "content/authors",
+                basePath: "/",
+                authorsPage: true,
+                sources: {
+                    local: true,
+                    // contentful: true,
+                },
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Novela by Narative`,
+                short_name: `Novela`,
+                start_url: `/`,
+                background_color: `#fff`,
+                theme_color: `#fff`,
+                display: `standalone`,
+                icon: `src/assets/favicon.png`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-netlify-cms`,
+            options: {},
+        },
+    ],
 };
